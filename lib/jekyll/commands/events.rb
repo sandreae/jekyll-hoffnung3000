0af54aebc7ddcc708790ed1cbcc26c5631c15e1c
sandreae <git@contact.samandreae.com>
2021-06-10 00:48:03 +0100

    Write events to _data folder as json

--- lib/jekyll/commands/events.rb
+++ lib/jekyll/commands/events.rb
@@ -14,9 +14,10 @@ module Jekyll
           
                 jq = JQ(response, parse_json: true)
                 
-                p jq.search('.data[] | {title: .title, description: .description, date: .date, time: {from: .slots[0].from, to: .slots[-1].to}, organiser: {name: .animal.userName, about: .organiser_biog}, venue: {id: null, name: .place.title, description: .place.description, area: .place.city }, imageUrl:.images[0].mediumImageUrl, tags:.tags, websiteUrl: .websiteUrl}')
+                data = jq.search('.data[] | {title: .title, description: .description, date: .date, time: {from: .slots[0].from, to: .slots[-1].to}, organiser: {name: .animal.userName, about: .organiser_biog}, venue: {id: null, name: .place.title, description: .place.description, area: .place.city }, imageUrl:.images[0].mediumImageUrl, tags:.tags, websiteUrl: .websiteUrl}')
       
-                # File.write("events.json", JSON.generate(data))
+                Dir.mkdir("_data") unless Dir.exists?("_data")
+                File.write("_data/events.json", JSON.generate(data))
               end
             end
           end
