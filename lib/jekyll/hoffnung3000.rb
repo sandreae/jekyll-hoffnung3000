98e08d2f3935840e46fad5a779a552adb72ed0d8
sandreae <git@contact.samandreae.com>
2021-06-10 00:21:05 +0100

    Initial commit

---
+++ lib/jekyll/hoffnung3000.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "hoffnung3000/version"
+
+module Jekyll
+  module Hoffnung3000
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/hoffnung3000/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Hoffnung3000
+    VERSION = "0.1.0"
+  end
+end
